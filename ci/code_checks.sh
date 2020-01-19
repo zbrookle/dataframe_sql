d7062fe975a461e412d0dcdf32dbb4eb6bce7e89
zbrookle <zachb1996@yahoo.com>
2020-01-18 19:22:15 -0500

    CLN: Code check fixes

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -1,3 +1,43 @@
+#!/bin/bash
+#
+# Run checks related to code quality.
+#
+# This script is intended for both the CI and to check locally that code standards are
+# respected. We are currently linting (PEP-8 and similar), looking for patterns of
+# common mistakes (sphinx directives with missing blank lines, old style classes,
+# unwanted imports...), we run doctests here (currently some files only), and we
+# validate formatting error in docstrings.
+#
+# Usage:
+#   $ ./ci/code_checks.sh               # run all checks
+#   $ ./ci/code_checks.sh lint          # run linting only
+#   $ ./ci/code_checks.sh patterns      # check for patterns that should not exist
+#   $ ./ci/code_checks.sh code          # checks on imported code
+#   $ ./ci/code_checks.sh doctests      # run doctests
+#   $ ./ci/code_checks.sh docstrings    # validate docstring errors
+#   $ ./ci/code_checks.sh dependencies  # check that dependencies are consistent
+#   $ ./ci/code_checks.sh typing	# run static type analysis
+
+[[ -z "$1" || "$1" == "lint" || "$1" == "patterns" || "$1" == "code" || "$1" == "doctests" || "$1" == "docstrings" || "$1" == "dependencies" || "$1" == "typing" ]] || \
+    { echo "Unknown command $1. Usage: $0 [lint|patterns|code|doctests|docstrings|dependencies|typing]"; exit 9999; }
+
+BASE_DIR="$(dirname $0)/.."
+RET=0
+CHECK=$1
+
+function invgrep {
+    # grep with inverse exist status and formatting for azure-pipelines
+    #
+    # This function works exactly as grep, but with opposite exit status:
+    # - 0 (success) when no patterns are found
+    # - 1 (fail) when the patterns are found
+    #
+    # This is useful for the CI, as we want to fail if one of the patterns
+    # that we want to avoid is found by grep.
+    grep -n "$@" | sed "s/^/$INVGREP_PREPEND/" | sed "s/$/$INVGREP_APPEND/" ; EXIT_STATUS=${PIPESTATUS[0]}
+    return $((! $EXIT_STATUS))
+}
+
 if [[ "$GITHUB_ACTIONS" == "true" ]]; then
     FLAKE8_FORMAT="##[error]%(path)s:%(row)s:%(col)s:%(code)s:%(text)s"
     INVGREP_PREPEND="##[error]"
@@ -30,7 +70,7 @@ if [[ -z "$CHECK" || "$CHECK" == "lint" ]]; then
 
     # Imports - Check formatting using isort see setup.cfg for settings
     MSG='Check import format using isort' ; echo $MSG
-    ISORT_CMD="isort --recursive --check-only pandas asv_bench"
+    ISORT_CMD="isort --recursive --check-only dataframe_sql"
     if [[ "$GITHUB_ACTIONS" == "true" ]]; then
         eval $ISORT_CMD | awk '{print "##[error]" $0}'; RET=$(($RET + ${PIPESTATUS[0]}))
     else
@@ -193,4 +233,4 @@ if [[ -z "$CHECK" || "$CHECK" == "typing" ]]; then
 fi
 
 
-exit $RET
+#exit $RET
