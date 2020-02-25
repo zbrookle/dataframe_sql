bddd05863b5ff6e85dd8cc85fb4306c7754e938d
zbrookle <zachb1996@yahoo.com>
2020-02-25 18:47:25 -0500

    ENH: Add versioneer versioning

--- dataframe_sql/__init__.py
+++ dataframe_sql/__init__.py
@@ -1,2 +1,6 @@
 # flake8: noqa
 from dataframe_sql.sql_select_query import query, register_temp_table, remove_temp_table
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 from pathlib import Path
+from versioneer import get_cmdclass, get_version
 
 CODE_DIRECTORY = Path(__file__).parent
 
 
-setup(name="dataframe_sql",)
+setup(name="dataframe_sql", version=get_version(), cmdclass=get_cmdclass())
