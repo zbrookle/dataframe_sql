7a595f5707b8661bbd78e0de7aef38f111beaa05
zbrookle <zachb1996@yahoo.com>
2020-02-24 22:17:09 -0500

    try changing invgrep

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -34,7 +34,7 @@ function invgrep {
     # This is useful for the CI, as we want to fail if one of the patterns
     # that we want to avoid is found by grep.
     grep -n "$@" | sed "s/^/$INVGREP_PREPEND/" | sed "s/$/$INVGREP_APPEND/" ; EXIT_STATUS=${PIPESTATUS[0]}
-    return $EXIT_STATUS
+    return $((! $EXIT_STATUS))
 }
 
 if [[ "$GITHUB_ACTIONS" == "true" ]]; then
