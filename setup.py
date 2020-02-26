d2c44a6bc04f8840c435c882d2fb14cd9e036b98
zbrookle <zachb1996@yahoo.com>
2020-02-25 19:13:43 -0500

    CLN: Fix typing hint issues

--- setup.py
+++ setup.py
@@ -7,17 +7,6 @@ from versioneer import get_cmdclass, get_version
 
 CODE_DIRECTORY = Path(__file__).parent
 
-print(find_packages())
-exit()
-
-SETUPTOOLS_KWARGS = {
-        "install_requires": [
-            "lark-parser == 0.8.1",
-            "pandas == 1.0.1"
-        ],
-        "zip_safe": False,
-    }
-
 setup(
     name="dataframe_sql",
     version=get_version(),
@@ -26,5 +15,5 @@ setup(
     maintainer_email="zachb1996@yahoo.com",
     description="A package for querying dataframes using SQL",
     python_requires=">=3.6.1",
-    **SETUPTOOLS_KWARGS
+    install_requires=["lark-parser == 0.8.1", "pandas == 1.0.1"]
 )
