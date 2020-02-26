f27b377da1d192a78edbc2b32f5e71629d6053e5
zbrookle <zachb1996@yahoo.com>
2020-02-25 19:00:41 -0500

    ENH: Add setup configuration

--- dataframe_sql/__init__.py
+++ dataframe_sql/__init__.py
@@ -2,5 +2,6 @@
 from dataframe_sql.sql_select_query import query, register_temp_table, remove_temp_table
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 from distutils.core import setup
 from pathlib import Path
+
+from setuptools import find_packages
+
 from versioneer import get_cmdclass, get_version
 
 CODE_DIRECTORY = Path(__file__).parent
 
+print(find_packages())
+exit()
 
-setup(name="dataframe_sql", version=get_version(), cmdclass=get_cmdclass())
+SETUPTOOLS_KWARGS = {
+        "install_requires": [
+            "lark-parser == 0.8.1",
+            "pandas == 1.0.1"
+        ],
+        "zip_safe": False,
+    }
+
+setup(
+    name="dataframe_sql",
+    version=get_version(),
+    cmdclass=get_cmdclass(),
+    maintainer="Zach Brookler",
+    maintainer_email="zachb1996@yahoo.com",
+    description="A package for querying dataframes using SQL",
+    python_requires=">=3.6.1",
+    **SETUPTOOLS_KWARGS
+)
