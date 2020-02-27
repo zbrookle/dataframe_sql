526f80d462568fbba6c9ac6b65b01dba4070851e
zbrookle <zachb1996@yahoo.com>
2020-02-26 19:49:00 -0500

    DOC: Add long description and source url

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
+import os
 from pathlib import Path
 
 from versioneer import get_cmdclass, get_version
 
 CODE_DIRECTORY = Path(__file__).parent
 
+
+def read_file(filename):
+    """Source the contents of a file"""
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+
 setup(
     name="dataframe_sql",
     version=get_version(),
     cmdclass=get_cmdclass(),
+    long_description=read_file("README.md"),
     maintainer="Zach Brookler",
     maintainer_email="zachb1996@yahoo.com",
     description="A package for querying dataframes using SQL",
@@ -25,4 +34,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
+    project_urls={
+        'Source': 'https://github.com/zbrookle/dataframe_sql',
+    },
 )
